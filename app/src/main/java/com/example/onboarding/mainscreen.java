86c8eac9f3be0f90253c3d526f50704b67c063f0
Saketh <saketh.tulasi@learner.manipal.edu>
2023-05-10 01:10:55 +0530

    Basic Login and StartUp Page

--- app/src/main/java/com/example/onboarding/mainscreen.java
+++ app/src/main/java/com/example/onboarding/mainscreen.java
@@ -1,14 +1,46 @@
 package com.example.onboarding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
-public class mainscreen extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class mainscreen extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ViewPagerAdapter2 adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+        tabLayout.addTab(tabLayout.newTab().setText("Startup"));
+        tabLayout.addTab(tabLayout.newTab().setText("Investor"));
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new ViewPagerAdapter2(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/onboarding/startup.java
@@ -0,0 +1,20 @@
+package com.example.onboarding;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class startup extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_startup, container, false);
+    }
+}
