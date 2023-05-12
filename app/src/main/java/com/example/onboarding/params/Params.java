fb3c7e256522c3df853bd5d2b8f74bcb2d4e123c
Saketh <saketh.tulasi@learner.manipal.edu>
2023-05-12 19:18:10 +0530

    DB SETUP

---
+++ app/src/main/java/com/example/onboarding/params/Params.java
@@ -0,0 +1,10 @@
+package com.example.onboarding.params;
+
+public class Params {
+    public static final int DB_VERSION=1;
+    public static final String DB_NAME="";
+    public static final String TABLE_NAME="";
+
+
+
+}
